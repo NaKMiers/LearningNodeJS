1a1523d575ed9a4d946bbf8fcfd9bd6adedf6822
AnK <56575187+NaKMiers@users.noreply.github.com>
2021-07-29 20:54:33 +0700

    comeback

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.engine('.hbs', handlebars({
   extname: '.hbs'
 }))
 app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname, 'resourses/views'))
+app.set('views', path.join(__dirname, 'resources/views'))
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -23,6 +23,14 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
   res.render('news');
+})
+
+app.get('/search', (req, res) => {
+  res.render('search')
+})
+
+app.post('/search', (req, res) => {
+  res.render('nguyen anh khoa')
 })
  
 app.listen(port, () => { 
