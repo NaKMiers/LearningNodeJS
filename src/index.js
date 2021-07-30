1764bcc19d40bcb2b6aa0de2d836c84170fea00d
AnK <56575187+NaKMiers@users.noreply.github.com>
2021-07-30 16:47:27 +0700

    asd

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 const port = 3000
 
-const app = express()
+const route = require('./routes') // auto get file index.js at routes folder
+
+const app = express() // create instance of express
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -23,21 +25,6 @@ app.engine('.hbs', handlebars({
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
-
-app.get('/search', (req, res) => {
-  res.render('search')
-})
-
-app.post('/search', (req, res) => {
-  console.log(req.body)
-  res.send('nguyen anh khoa')
-})
+route(app)
  
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
